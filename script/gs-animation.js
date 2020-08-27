5bd880e5fe65367f42f25e82d78decf31b9718ab
iamSK77 <sk77.mobilefix@gmail.com>
2020-08-27 13:02:07 +0530

    Added GSAP Animation

--- script/gs-animation.js
+++ script/gs-animation.js
@@ -1,4 +1,4 @@
-gsap.from('.hero-left', {opacity: 0, duration: 1, y: -100});
+gsap.from('.hero-left', {opacity: 0, duration: 1.3, y: -100});
 gsap.from('.anim1', {opacity: 0, duration: 1, delay: .5, y: -30});
 gsap.from('.hero-right', {opacity: 0, duration: 1, delay: .5, x: 100});
-gsap.from('.scroll-down', {opacity: 0, duration: 1, y: -30});
+gsap.from('.scroll-down', {opacity: 0, duration: 1, y: -50});
