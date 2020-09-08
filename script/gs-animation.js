91d552a4afa57ac60e7c6efa1ab9fc4864ee574a
iamSK77 <sk77.mobilefix@gmail.com>
2020-09-08 21:44:30 +0530

    Fixed animation bug

--- script/gs-animation.js
+++ script/gs-animation.js
@@ -1,4 +1,6 @@
-gsap.from('.hero-left', {opacity: 0, duration: 2.5, ease: Back.easeOut.config(1.9), y: -100});
-gsap.from('.anim1', {opacity: 0, duration: 1, delay: .5, y: -30});
-gsap.from('.hero-right', {opacity: 0, duration: 1, delay: .5, x: 100});
-gsap.from('.scroll-down', {opacity: 0, duration: 1, y: -50});
+window.addEventListener('load', () => {
+    gsap.from('.hero-left', {opacity: 0, duration: 2.5, ease: Back.easeOut.config(1.9), y: -100});
+    gsap.from('.anim1', {opacity: 0, duration: 1, delay: .5, y: -30});
+    gsap.from('.hero-right', {opacity: 0, duration: 1, delay: .5, x: 100});
+    gsap.from('.scroll-down', {opacity: 0, duration: 1, y: -50});
+});
