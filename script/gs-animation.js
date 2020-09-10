4a4ded17d412ce11c1dfaee24be251182cedb107
iamSK77 <sk77.mobilefix@gmail.com>
2020-09-10 23:21:00 +0530

    minor bug fixes

--- script/gs-animation.js
+++ script/gs-animation.js
@@ -4,5 +4,5 @@ window.addEventListener('load', () => {
         gsap.from('.anim1', {opacity: 0, duration: 1, delay: .5, y: -30});
         gsap.from('.hero-right', {opacity: 0, duration: 1, delay: .5, x: 100});
         gsap.from('.scroll-down', {opacity: 0, duration: 1, y: -50});
-    }, 500);
+    }, 200);
 });
